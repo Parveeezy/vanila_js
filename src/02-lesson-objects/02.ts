2197cb87eab0976d8faffda8c66044f58d8addf9
Parveeezy <parveeezy@gmail.com>
2022-09-15 22:12:53 +0300

    02-lesson-test-objects

---
+++ src/02-lesson-objects/02.ts
@@ -0,0 +1,48 @@
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Parviz',
+    age: 29,
+    isActive: false,
+    address: {
+        streetTitle: 'Kuprina 43',
+        city: {
+            title: 'Moscow',
+            countryTitle: 'Russia'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML'
+        },
+        {
+            id: 2,
+            title: 'CSS'
+        },
+        {
+            id: 3,
+            title: 'REACT'
+        },
+    ]
+}
---
+++ src/02-lesson-objects/02_02.ts
@@ -0,0 +1,27 @@
+type StreetType = {
+    title: string
+}
+type AddressType = {
+    number?: number
+    street: StreetType
+}
+type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+
+type governmentBuildingType = {
+    type: 'HOSPITAL' | 'FIRE-STATION'
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<governmentBuildingType>
+    citizensNumber: number
+}
